644dd9e1f1fdc24bf413d964d53974a254379428
Matthew Kerr <matthewkerr123@gmail.com>
2024-01-24 23:13:10 +1300

    Add files via upload

---
+++ aws_jedi_council_static_www/_next/static/ruHPjmNorsVTrpPmjd2NH/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/e7d3bab42d9af29d.css","static/chunks/pages/index-a7c9e7502d68e37a.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
